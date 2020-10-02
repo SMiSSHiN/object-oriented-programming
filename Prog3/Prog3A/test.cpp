4ae3ca347d4660c63caa0d49d004dfb7820eba24
Burgos1337 <dima@sploit00n.com>
2020-10-02 21:41:38 +0300

    resources added

--- Prog3/Prog3A/test.cpp
+++ Prog3/Prog3A/test.cpp
@@ -11,8 +11,10 @@ TEST(carriage_constructor, default_constructor) {
 }
 
 TEST(carriage_constructor, initial_constructor) {
-    ASSERT_NO_THROW(carriage main_carriage_1(14, 8););
-    ASSERT_ANY_THROW(carriage main_carriage_2(5, 9););
+    ASSERT_NO_THROW(carriage main_carriage_1(14, 8));
+    ASSERT_ANY_THROW(carriage main_carriage_2(5, 9));
+    ASSERT_ANY_THROW(carriage main_carriage_3(-2, 3));
+    ASSERT_ANY_THROW(carriage main_carriage_2(7, -4));
 }
 
 TEST(carriage_method, setter) {
@@ -20,6 +22,10 @@ TEST(carriage_method, setter) {
     ASSERT_NO_THROW(main_carriage_1.set_carriage(12, 5));
     carriage main_carriage_2;
     ASSERT_ANY_THROW(main_carriage_2.set_carriage(6, 13));
+    carriage main_carriage_3;
+    ASSERT_ANY_THROW(main_carriage_3.set_carriage(-3, 18));
+    carriage main_carriage_4;
+    ASSERT_ANY_THROW(main_carriage_4.set_carriage(15, -9));
 }
 
 TEST(train_constructor, default_constructor) {
@@ -31,112 +37,127 @@ TEST(train_constructor, default_constructor) {
 }
 
 TEST(train_constructor, initial_constructor) {
-    carriage main_carriage_0;
     carriage main_carriage_1(7, 3);
     carriage main_carriage_2(12, 7);
-    struct carriage cars[3];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    cars[2] = main_carriage_2;
+    struct carriage cars[2];
+    cars[0] = main_carriage_1;
+    cars[1] = main_carriage_2;
     train main_train_1(cars, 3);
     ASSERT_NO_THROW(main_train_1);
     ASSERT_EQ(3, main_train_1.get_length());
-    ASSERT_TRUE(0 == memcmp(cars, main_train_1.get_carriages(), sizeof(cars)));
     ASSERT_ANY_THROW(train main_train_2(cars, 0));
+    ASSERT_ANY_THROW(train main_train_3(cars, 108));
 }
 
 TEST(train_methods, basic_methods) {
-    carriage main_carriage_0;
     carriage main_carriage_1(6, 4);
     carriage main_carriage_2(11, 5);
-    struct carriage cars[3];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    cars[2] = main_carriage_2;
+    struct carriage cars[2];
+    cars[0] = main_carriage_1;
+    cars[1] = main_carriage_2;
     train main_train_1;
     ASSERT_NO_THROW(main_train_1.set_length(3));
     ASSERT_EQ(3, main_train_1.get_length());
     ASSERT_NO_THROW(main_train_1.set_carriages(cars));
-    ASSERT_TRUE(0 == memcmp(cars, main_train_1.get_carriages(), sizeof(cars)));
     train main_train_2;
     ASSERT_ANY_THROW(main_train_2.set_length(-2));
+    train main_train_3;
+    ASSERT_ANY_THROW(main_train_3.set_length(134));
+}
+
+TEST(train_methods, input) {
+    carriage main_carriage_0;
+    carriage main_carriage_1(12, 7);
+    carriage main_carriage_2(14, 5);
+    carriage main_carriage_3(11, 6);
+    struct carriage cars[4];
+    cars[0] = main_carriage_0;
+    cars[1] = main_carriage_1;
+    cars[2] = main_carriage_2;
+    cars[3] = main_carriage_3;  
+    train main_train;
+    std::stringstream ss_1, ss_2, ss_3;
+    ss_1 << "-2";
+    ASSERT_ANY_THROW(ss_1 >> main_train);
+    ss_2 << "120";
+    ASSERT_ANY_THROW(ss_2 >> main_train);
+    ss_3 << "3 12 7 14 5 11 6";
+    ASSERT_NO_THROW(ss_3 >> main_train);
+    ASSERT_EQ(4, main_train.get_length());
+    ASSERT_TRUE(0 == memcmp(cars, main_train.get_carriages(), sizeof(cars)));
 }
 
 TEST(train_methods, output) {
-    carriage main_carriage_0;
-    carriage main_carriage_1(5, 2);
-    struct carriage cars[2];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    train main_train(cars, 2);
+    carriage main_carriage(5, 2);
+    train main_train;
+    main_train += main_carriage;
     std::stringstream ss;
     ss << main_train;
     ASSERT_EQ("Carriage #0 occupancy: 0 passenger(s)\nCarriage #1 occupancy: 2 passenger(s)\n", ss.str());
 }
 
 TEST(train_methods, occupy_seats) {
-    carriage main_carriage_0;
-    carriage main_carriage_1(18, 7);
-    struct carriage cars[2];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    train main_train(cars, 2);
-    main_train(1, 5);
+    carriage main_carriage(18, 7);
+    train main_train;
+    main_train += main_carriage;
+    ASSERT_ANY_THROW(main_train(-3, 7));
+    ASSERT_ANY_THROW(main_train(1, -6));
+    ASSERT_ANY_THROW(main_train(3, 8));
+    ASSERT_ANY_THROW(main_train(1, 15));
+    ASSERT_NO_THROW(main_train(1, 5));
     ASSERT_EQ(12, main_train.get_carriages()[1].occupied_seats_number);
 }
 
 TEST(train_methods, carriage_occupancy_information) {
-    carriage main_carriage_0;
-    carriage main_carriage_1(22, 15);
-    struct carriage cars[2];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    train main_train(cars, 2);
+    carriage main_carriage(22, 15);
+    train main_train;
+    main_train += main_carriage;
+    ASSERT_ANY_THROW(main_train[-2]);
+    ASSERT_ANY_THROW(main_train[7]);
+    ASSERT_NO_THROW(main_train[1]);
     ASSERT_EQ(15, main_train[1]);
 }
 
 TEST(train_methods, add_carriage) {
-    carriage main_carriage_0;
     carriage main_carriage_1(9, 4);
     carriage main_carriage_2(27, 16);
+    carriage main_carriage_3(18, 13);
     struct carriage cars[2];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    train main_train(cars, 2);
-    main_train += main_carriage_2;
-    ASSERT_EQ(3, main_train.get_length());
-    ASSERT_EQ(27, main_train.get_carriages()[2].maximum_capacity);
-    ASSERT_EQ(16, main_train.get_carriages()[2].occupied_seats_number);
+    cars[0] = main_carriage_1;
+    cars[1] = main_carriage_2;
+    train main_train(cars, 3);
+    main_train += main_carriage_3;
+    ASSERT_EQ(4, main_train.get_length());
+    ASSERT_EQ(18, main_train.get_carriages()[3].maximum_capacity);
+    ASSERT_EQ(13, main_train.get_carriages()[3].occupied_seats_number);
 }
 
 TEST(train_methods, train_unhook) {
-    carriage main_carriage_0;
     carriage main_carriage_1(32, 17);
     carriage main_carriage_2(23, 14);
     carriage main_carriage_3(45, 22);
     carriage main_carriage_4(19, 11);
     struct carriage cars[4];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    cars[2] = main_carriage_2;
-    cars[3] = main_carriage_3;
-    cars[4] = main_carriage_4;
+    cars[0] = main_carriage_1;
+    cars[1] = main_carriage_2;
+    cars[2] = main_carriage_3;
+    cars[3] = main_carriage_4;
     train main_train(cars, 5);
     int min_train[2] = { 2, 4 };
-    main_train( 2, min_train );
+    ASSERT_ANY_THROW(main_train( -7, min_train ));
+    ASSERT_ANY_THROW(main_train( 9, min_train ));
+    ASSERT_NO_THROW(main_train( 2, min_train ));
     ASSERT_EQ(3, main_train.get_length());
     ASSERT_EQ(45, main_train.get_carriages()[2].maximum_capacity);
     ASSERT_EQ(22, main_train.get_carriages()[2].occupied_seats_number);
 }
 
 TEST(train_methods, passengers_number) {
-    carriage main_carriage_0;
     carriage main_carriage_1(8, 3);
     carriage main_carriage_2(49, 36);
-    struct carriage cars[3];
-    cars[0] = main_carriage_0;
-    cars[1] = main_carriage_1;
-    cars[2] = main_carriage_2;
+    struct carriage cars[2];
+    cars[0] = main_carriage_1;
+    cars[1] = main_carriage_2;
     train main_train(cars, 3);
     ASSERT_EQ(39, main_train.get_passengers_number());
 }
--- Prog3/Prog3A/train.cpp
+++ Prog3/Prog3A/train.cpp
@@ -13,7 +13,7 @@ namespace Prog3A {
     }
     
     carriage& carriage::set_carriage(int maximum_capacity, int occupied_seats_number) {
-        if (maximum_capacity < 0 || occupied_seats_number < 0 || occupied_seats_number > maximum_capacity) {
+        if (maximum_capacity < 0 || occupied_seats_number < 0 || maximum_capacity < occupied_seats_number) {
            throw "Invalid carriage parameters!";
         }
         
@@ -29,7 +29,7 @@ namespace Prog3A {
     }
     
     train::train(struct carriage *cars, int length) {
-        if (length < 1) {
+        if (length < 1 || length > 100) {
            throw "Invalid train length!";
         }
         
@@ -38,7 +38,7 @@ namespace Prog3A {
     }
     
     train& train::set_length(int length) {
-        if (length < 1) {
+        if (length < 1 || length > 100) {
            throw "Invalid train length!";
         }
 
@@ -49,8 +49,8 @@ namespace Prog3A {
     
     train& train::set_carriages(struct carriage *cars) {
         this->cars[0] = { 0, 0 };
-        for (int i = 1; i < this->length; i++) {
-            this->cars[i] = cars[i];
+        for (int i = 0; i < this->length - 1; i++) {
+            this->cars[i + 1] = cars[i];
         }
         
         return *this;
@@ -65,17 +65,24 @@ namespace Prog3A {
     }
     
     std::istream& operator >> (std::istream& in, train& main_train) {
-        int maximum_capacity, occupied_seats_number;
-        
-        in >> maximum_capacity;
-        in >> occupied_seats_number;
-        if (occupied_seats_number > maximum_capacity) {
-           throw "Invalid carriage parameters!";
+        int maximum_capacity, occupied_seats_number, carriages_number;
+
+        in >> carriages_number;
+        if (carriages_number < 1 || carriages_number > 100) {
+           throw "Invalid carriages number!";
         }
         
-        main_train.cars[main_train.length].maximum_capacity = maximum_capacity;
-        main_train.cars[main_train.length].occupied_seats_number = occupied_seats_number;
-        main_train.length++;
+        for (int i = 0; i < carriages_number; i++) {
+            in >> maximum_capacity;
+            in >> occupied_seats_number;
+            if (maximum_capacity < occupied_seats_number) {
+               throw "Invalid carriage parameters!";
+            }
+        
+            main_train.cars[main_train.length].maximum_capacity = maximum_capacity;
+            main_train.cars[main_train.length].occupied_seats_number = occupied_seats_number;
+            main_train.length++;
+        }
         
         return in;
     }
@@ -89,21 +96,32 @@ namespace Prog3A {
     }
     
     train& train::operator () (int carriage_index, int seats_number) {
-        if (seats_number < this->cars[carriage_index].maximum_capacity - this->cars[carriage_index].occupied_seats_number) {
-           this->cars[carriage_index].occupied_seats_number += seats_number;
+        if (carriage_index < 1 || carriage_index > this->length - 1) {
+           throw "Invalid carriage index!";
         }
-        else {
-           this->cars[carriage_index].occupied_seats_number = this->cars[carriage_index].maximum_capacity;
+
+        if (seats_number < 0 || seats_number >= this->cars[carriage_index].maximum_capacity - this->cars[carriage_index].occupied_seats_number) {
+           throw "Invalid seats number!";
         }
+
+        this->cars[carriage_index].occupied_seats_number += seats_number;
         
         return *this;
     }
     
     int& train::operator [] (int carriage_index) {
+        if (carriage_index < 1 || carriage_index > this->length - 1) {
+           throw "Invalid carriage index!";
+        }
+        
         return this->cars[carriage_index].occupied_seats_number;
     }
     
     train& train::operator += (carriage& main_carriage) {
+        if (this->length == 100) {
+           throw "Unable to add carriage!";          
+        }
+        
         this->cars[this->length] = main_carriage;
         this->length++;
         
@@ -111,6 +129,10 @@ namespace Prog3A {
     }
     
     train& train::operator ( ) (int carriages_number, int *min_train) {
+        if (carriages_number < 1 || carriages_number > this->length - 1) {
+           throw "Invalid carriages number!";
+        }
+
         for (int i = 0; i < carriages_number; i++) {
             for (int j = min_train[i] - i; j < this->length; j++) {
                 cars[j] = cars[j + 1];
